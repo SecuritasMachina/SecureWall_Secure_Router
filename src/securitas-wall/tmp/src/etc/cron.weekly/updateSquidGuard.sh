e9df80d8600748348fc601512fc7970b2b414fe8
Ackdev Support <help@ackdev.com>
2019-11-24 14:04:48 -0500

    Add domains download

--- src/securitas-wall/tmp/src/etc/cron.weekly/updateSquidGuard.sh
+++ src/securitas-wall/tmp/src/etc/cron.weekly/updateSquidGuard.sh
@@ -1,12 +1,14 @@
 echo "Download Black & White lists"
-wget -q -O /tmp/mesd_blacklists.tgz http://squidguard.mesd.k12.or.us/blacklists.tgz
-wget -q -O /tmp/securitasmachina.tgz https://github.com/SecuritasMachina/SecuritasMachina-Distrib/raw/master/distrib/blacklists/securitasmachina.tgz
-wget -q -O /var/lib/squid/bumpdomains.lst https://github.com/SecuritasMachina/SecuritasMachina-Distrib/raw/master/distrib/whitelists/bumpdomains.lst
+wget -O /tmp/mesd_blacklists.tgz http://squidguard.mesd.k12.or.us/blacklists.tgz
+wget -O /tmp/securitasmachina.tgz https://github.com/SecuritasMachina/SecuritasMachina-Distrib/raw/master/distrib/blacklists/securitasmachina.tgz
+wget -O /var/lib/squid/bumpdomains.lst https://github.com/SecuritasMachina/SecuritasMachina-Distrib/raw/master/distrib/whitelists/bumpdomains.lst
 
 echo "Extracting"
 tar -xzf /tmp/mesd_blacklists.tgz -C /var/lib/squidguard/db
 tar -xzf /tmp/securitasmachina.tgz -C /var/lib/squidguard/db/blacklists
 wget -O /var/lib/squidguard/db/blacklists/trackers/urls https://raw.githubusercontent.com/SecuritasMachina/SecuritasMachina-Distrib/master/sources/blackLists/squidguard/trackers/urls
+wget -O /var/lib/squidguard/db/blacklists/trackers/domains https://raw.githubusercontent.com/SecuritasMachina/SecuritasMachina-Distrib/master/sources/blackLists/squidguard/trackers/domains
+
 
 chown -R proxy:proxy /var/lib/squidguard/db/*
 
