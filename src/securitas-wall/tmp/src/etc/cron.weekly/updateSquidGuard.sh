51115567bb0e6a293e383dfddfdb222aa957a6cd
SecuritasMachina <5593482+SecuritasMachina@users.noreply.github.com>
2019-11-24 19:03:54 -0500

    Update updateSquidGuard.sh
    Add open source advertiser blacklists

--- src/securitas-wall/tmp/src/etc/cron.weekly/updateSquidGuard.sh
+++ src/securitas-wall/tmp/src/etc/cron.weekly/updateSquidGuard.sh
@@ -1,4 +1,5 @@
 echo "Download Black & White lists"
+mkdir -p /var/lib/squid/bumpdomains.lst
 wget -O /tmp/mesd_blacklists.tgz http://squidguard.mesd.k12.or.us/blacklists.tgz
 wget -O /tmp/securitasmachina.tgz https://github.com/SecuritasMachina/SecuritasMachina-Distrib/raw/master/distrib/blacklists/securitasmachina.tgz
 wget -O /var/lib/squid/bumpdomains.lst https://github.com/SecuritasMachina/SecuritasMachina-Distrib/raw/master/distrib/whitelists/bumpdomains.lst
@@ -6,9 +7,16 @@ wget -O /var/lib/squid/bumpdomains.lst https://github.com/SecuritasMachina/Secur
 echo "Extracting"
 tar -xzf /tmp/mesd_blacklists.tgz -C /var/lib/squidguard/db
 tar -xzf /tmp/securitasmachina.tgz -C /var/lib/squidguard/db/blacklists
+
+mkdir -p /var/lib/squidguard/db/blacklists/trackers
+mkdir -p /var/lib/squidguard/db/blacklists/trackers2
+
 wget -O /var/lib/squidguard/db/blacklists/trackers/urls https://raw.githubusercontent.com/SecuritasMachina/SecuritasMachina-Distrib/master/sources/blackLists/squidguard/trackers/urls
 wget -O /var/lib/squidguard/db/blacklists/trackers/domains https://raw.githubusercontent.com/SecuritasMachina/SecuritasMachina-Distrib/master/sources/blackLists/squidguard/trackers/domains
 wget -O /var/lib/squidguard/db/blacklists/trackers/expressions https://raw.githubusercontent.com/SecuritasMachina/SecuritasMachina-Distrib/master/sources/blackLists/squidguard/trackers/expressions
+wget -o /var/lib/squidguard/db/blacklists/trackers2/urls https://raw.githubusercontent.com/olbat/ut1-blacklists/master/blacklists/publicite/urls
+wget -o /var/lib/squidguard/db/blacklists/trackers2/domains https://raw.githubusercontent.com/olbat/ut1-blacklists/master/blacklists/publicite/domains
+wget -o /var/lib/squidguard/db/blacklists/trackers2/expressions https://raw.githubusercontent.com/olbat/ut1-blacklists/master/blacklists/publicite/expressions 
 
 chown -R proxy:proxy /var/lib/squidguard/db/*
 
