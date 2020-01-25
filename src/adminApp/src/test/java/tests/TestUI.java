e948fe8680a231e54d9c9f29f71709d4b71a72a9
Your Name <jaxtrx>
2020-01-25 17:50:26 -0500

    Move webadmin

--- src/adminApp/src/test/java/tests/TestUI.java
+++
@@ -1,7 +0,0 @@
-package tests;
-
-public class TestUI {
-	// https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java
-//	compile group: 'org.seleniumhq.selenium', name: 'selenium-java', version: '3.141.59'
-
-}
