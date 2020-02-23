7ec7a887911ef49ef04c7fb2c505d217bbdb53c7
Your Name <jaxtrx>
2020-02-23 17:56:06 -0500

    Add Information on why a url was blocked

--- src/securitas-wall-webadmin/tmp/src/var/www/html/blocked.php
+++ src/securitas-wall-webadmin/tmp/src/var/www/html/blocked.php
@@ -1,6 +1,14 @@
 <?php
-
 header('HTTP/1.0 403 Forbidden');
-echo 'Blocked';
-exit;
 ?>
+<html>
+<head><title>Blocked</title>
+</head>
+<body>
+<?php
+    echo 'Blocked URL: ' . $_GET['url'] . '<br/>';
+    echo 'Client: ' . $_GET['clientaddr'] . '<br/>';
+    echo 'Rule: ' . $_GET['destinationgroup'] . '<br/>';
+?>
+</body>
+</html>
