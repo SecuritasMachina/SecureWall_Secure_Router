c5bf077d9f82e49068f16486201c4990c5f83c47
Ackdev Support <help@ackdev.com>
2020-02-24 13:52:04 -0500

    Improving Virus tests

---
+++ src/securitas-wall/tmp/src/usr/local/bin/testVirus.sh
@@ -0,0 +1,88 @@
+set -x
+
+proxy="http://localhost:3128"
+httpWebSite="http://securitasMachina.com"
+httpsWebSite="https://securitasMachina.com"
+
+if curl -s --proxy $proxy $httpWebSite | grep "ProxyTest1979"
+then
+	step= "Success $httpWebSite !"
+else
+	step= "Fail !"
+fi
+
+if curl -s --proxy $proxy $httpsWebSite | grep "ProxyTest1979"
+then
+	step= "Success $httpsWebSite !"
+else
+	step= "Fail !"
+fi
+
+if curl -k -s --proxy $proxy $httpsWebSite | grep "ProxyTest1979"
+then
+	step= "Success! - $proxy $httpsWebSite"
+else
+	step= "Fail! - $proxy $httpsWebSite"
+fi
+
+if curl -s --proxy $proxy http://www.eicar.org/download/eicar.com | grep "EICAR-STANDARD-ANTIVIRUS-TEST-FILE"
+then
+	step= "Expected: Fail! $proxy http://www.eicar.org/download/eicar.com"
+else
+	step= "Success - Blocked virus!"
+fi
+
+if curl -s -k --proxy $proxy https://secure.eicar.org/eicar.com | grep "EICAR-STANDARD-ANTIVI"
+then
+	step= "Failure Expected, not using ssl port - $proxy https://secure.eicar.org/eicar.com"
+else
+	step= "Success - Blocked virus!"
+fi
+
+if curl -s -k --proxy $proxy https://secure.eicar.org/eicar.com | grep "EICAR-STANDARD-ANTIVI"
+then
+	step= "Fail! - $proxy https://secure.eicar.org/eicar.com"
+else
+	step= "Success - Blocked virus!"
+fi
+
+if curl -s -k --proxy $proxy https://secure.eicar.org/eicar_com.zip | grep "ANTIVI"
+then
+	step= "Fail! - $proxy https://secure.eicar.org/eicar.com"
+else
+	step= "Success - Blocked virus!"
+fi
+
+if curl -s -k --proxy $proxy https://secure.eicar.org/eicarcom2.zip | grep "ANTIVI"
+then
+	step= "Fail! - $proxy https://secure.eicar.org/eicar.com"
+else
+	step= "Success - Blocked virus!"
+fi
+
+if curl -s -k --proxy $proxy http://wildfire.paloaltonetworks.com/publicapi/test/pe | grep -i "block"
+then
+	step= "Success - Blocked virus!"
+else
+	step= "Fail!"
+fi
+
+step= "Testing Virus Malware groups"
+while read NAME
+do
+    step "Testing $NAME"
+    value = "http://securitasmachina.com/sampleViruses/$NAME"
+    if curl -s -k --proxy $proxy $value | grep -i "block"
+	then
+		step= "Success - Blocked virus @ $value"
+	else
+		step= "Fail! @ $value"
+	fi
+    
+done < virusNames.lst
+
+#https://wildfire.paloaltonetworks.com/publicapi/test/pe
+
+
+
+
