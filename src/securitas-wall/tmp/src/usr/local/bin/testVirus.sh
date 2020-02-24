f1fdb74d2e7bc20334bd5d6fdcf8ed70231cd70e
Ackdev Support <help@ackdev.com>
2020-02-24 14:12:26 -0500

    Shrink proxy virus test script

--- src/securitas-wall/tmp/src/usr/local/bin/testVirus.sh
+++ src/securitas-wall/tmp/src/usr/local/bin/testVirus.sh
@@ -4,68 +4,38 @@ proxy="http://localhost:3128"
 httpWebSite="http://securitasMachina.com"
 httpsWebSite="https://securitasMachina.com"
 
-if curl -s --proxy $proxy $httpWebSite | grep "ProxyTest1979"
-then
-	step= "Success $httpWebSite !"
-else
-	step= "Fail !"
-fi
+# Declare a string array with type
+declare -a StringArray=("$httpWebSite" \
+"$httpsWebSite")
+ 
+# Read the array values with space
+for val in "${StringArray[@]}"; do
+	if curl -s -k --proxy $proxy $val | grep -i "ProxyTest1979"
+	then
+		step= "Success $val"
+	else
+		step= "Fail $val !"
+	fi
+done
 
-if curl -s --proxy $proxy $httpsWebSite | grep "ProxyTest1979"
-then
-	step= "Success $httpsWebSite !"
-else
-	step= "Fail !"
-fi
 
-if curl -k -s --proxy $proxy $httpsWebSite | grep "ProxyTest1979"
-then
-	step= "Success! - $proxy $httpsWebSite"
-else
-	step= "Fail! - $proxy $httpsWebSite"
-fi
-
-if curl -s --proxy $proxy http://www.eicar.org/download/eicar.com | grep "EICAR-STANDARD-ANTIVIRUS-TEST-FILE"
-then
-	step= "Expected: Fail! $proxy http://www.eicar.org/download/eicar.com"
-else
-	step= "Success - Blocked virus!"
-fi
-
-if curl -s -k --proxy $proxy https://secure.eicar.org/eicar.com | grep "EICAR-STANDARD-ANTIVI"
-then
-	step= "Failure Expected, not using ssl port - $proxy https://secure.eicar.org/eicar.com"
-else
-	step= "Success - Blocked virus!"
-fi
-
-if curl -s -k --proxy $proxy https://secure.eicar.org/eicar.com | grep "EICAR-STANDARD-ANTIVI"
-then
-	step= "Fail! - $proxy https://secure.eicar.org/eicar.com"
-else
-	step= "Success - Blocked virus!"
-fi
-
-if curl -s -k --proxy $proxy https://secure.eicar.org/eicar_com.zip | grep "ANTIVI"
-then
-	step= "Fail! - $proxy https://secure.eicar.org/eicar.com"
-else
-	step= "Success - Blocked virus!"
-fi
-
-if curl -s -k --proxy $proxy https://secure.eicar.org/eicarcom2.zip | grep "ANTIVI"
-then
-	step= "Fail! - $proxy https://secure.eicar.org/eicar.com"
-else
-	step= "Success - Blocked virus!"
-fi
-
-if curl -s -k --proxy $proxy http://wildfire.paloaltonetworks.com/publicapi/test/pe | grep -i "block"
-then
-	step= "Success - Blocked virus!"
-else
-	step= "Fail!"
-fi
+# Declare a string array with type
+declare -a StringArray=("http://www.eicar.org/download/eicar.com" \
+"https://secure.eicar.org/eicar.com" \
+"https://secure.eicar.org/eicar_com.zip" \
+"https://secure.eicar.org/eicarcom2.zip" \
+"http://wildfire.paloaltonetworks.com/publicapi/test/pe" \
+"https://wildfire.paloaltonetworks.com/publicapi/test/pe")
+ 
+# Read the array values with space
+for val in "${StringArray[@]}"; do
+	if curl -s -k --proxy $proxy $val | grep -i "block"
+	then
+		step= "Success - Blocked virus!"
+	else
+		step= "Fail!"
+	fi
+done
 
 step= "Testing Virus Malware groups"
 while read NAME
@@ -78,7 +48,6 @@ do
 	else
 		step= "Fail! @ $value"
 	fi
-    
 done < virusNames.lst
 
 #https://wildfire.paloaltonetworks.com/publicapi/test/pe
