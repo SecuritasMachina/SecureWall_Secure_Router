15d80a6f0d9add0ad55959529c68c7e506008759
Your Name <jaxtrx>
2020-02-24 21:17:29 -0500

    Parrellize script

--- src/securitas-wall/tmp/src/usr/local/bin/testVirus.sh
+++ src/securitas-wall/tmp/src/usr/local/bin/testVirus.sh
@@ -1,55 +1,35 @@
-#set -x
+#!/bin/bash
 
 proxy="http://localhost:13081"
 http_proxy=$proxy
 httpWebSite="http://securitasMachina.com"
 httpsWebSite="https://securitasMachina.com"
 results=""
-step1="none"
-# Declare a string array with type
-declare -a StringArray=("$httpWebSite" \
-"$httpsWebSite")
- 
+testVirus(){
+    if wget -qO- $1 | grep -i "Blocked"
+        then
+                echo "Success $2"
+        else
+                echo "Fail $2"
+        fi
+}
+declare -a StringArray=("$httpWebSite" "$httpsWebSite")
 # Read the array values with space
 for val in "${StringArray[@]}"; do
-	if curl -s -k --proxy $proxy $val | grep -i "ProxyTest1979"
-	then
-		results="$results Success $val\n"
-	else
-		results="$results Fail $val\n"
-	fi
+	testVirus $val $val &
 done
-
-
-# Declare a string array with type
 declare -a StringArray=("http://www.eicar.org/download/eicar.com" \
 "https://secure.eicar.org/eicar.com" \
 "https://secure.eicar.org/eicar_com.zip" \
 "https://secure.eicar.org/eicarcom2.zip" \
 "http://wildfire.paloaltonetworks.com/publicapi/test/pe" \
 "https://wildfire.paloaltonetworks.com/publicapi/test/pe")
- 
-# Read the array values with space
 for val in "${StringArray[@]}"; do
-	if wget -qO- $val | grep -i "Blocked"
-	then
-		results="$results Success $val\n"
-	else
-		results="$results Fail $val\n"
-	fi
+	testVirus $val $val &
 done
-
-echo "Testing Virus Malware groups"
 while read NAME
 do
     value1="http://securitasmachina.com/sampleViruses/Malz/$NAME"
-
-    if wget -qO- $value1 | grep -i "Blocked"
-	then
-		results="$results Success $NAME\n"
-	else
-		results="$results Fail $NAME\n"
-	fi
+	testVirus $value1 $NAME &
 done < /usr/local/bin/virusNames.lst
-
-echo -e $results
+wait
